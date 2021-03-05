c42583980f2aa2982b29912662bfc1cdd0c43278
HembitskiAndrei <shaftdg3@gmail.com>
2021-03-05 19:32:20 +0300

    added rules scene

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -43,12 +43,14 @@ export const BUTTON_NUMBER_STYLE = {
   color: "#333333",
   align: "center",
 };
+
 export const RULES_TEXT =
   "Phasellus tempor velit tellus, tempus facilisis sem placerat et. Maecenas et risus in tortor bibendum laoreet. Vestibulum vel augue nec erat dignissim ullamcorper vel vel dui. Fusce rutrum libero ornare dolor luctus, sit amet eleifend elit rutrum. \n" +
   "\n" +
   "Sed tincidunt urna eu leo gravida hendrerit. Nam imperdiet fermentum felis, venenatis mattis justo ultrices vitae. Maecenas odio orci, vehicula in egestas a, ultrices quis risus. Etiam posuere rhoncus erat, in suscipit erat dignissim at. \n" +
   "\n" +
   "Nam imperdiet fermentum felis, venenatis mattis justo ultrices vitae. Maecenas odio orci, vehicula in egestas a, ultrices quis risus. Etiam posuere rhoncus erat, in suscipit erat dignissim at.";
+
 export const TEXT_AREA_CONFIG_FOR_RULES = {
   x: 100,
   y: 75,
