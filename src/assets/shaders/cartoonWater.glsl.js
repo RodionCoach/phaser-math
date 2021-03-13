edba270f3da47f4c1cc1c50f87528bf79bef58e6
HembitskiAndrei <shaftdg3@gmail.com>
2021-03-13 18:16:40 +0300

    changed moving lilies - used tweens changed style of lilies text

--- src/assets/shaders/cartoonWater.glsl.js
+++ src/assets/shaders/cartoonWater.glsl.js
@@ -21,7 +21,8 @@ void main( void ) {
     float scattering = 0.2;
     float wavespeed = 0.15;
     float intensity = 0.4;
-    float displacement = max((length(texture2D(iChannel1, vec2(vUv.x - 0.15, vUv.y * 0.85 - time * 0.35) * 20.0 * scattering * wavespeed)) * intensity) - 0.1, 0.0);
+    vec3 colorNoise = texture2D(iChannel1, vec2(vUv.x, vUv.y - time * 0.15) * 0.35).rgb * 0.2;
+    float displacement = max((length(texture2D(iChannel1, vec2(vUv.x * cos(cos(time) * 0.3) - 0.15, vUv.y * cos(sin(time) * 0.35) * 0.85 - time * 0.35) * 20.0 * scattering * wavespeed + colorNoise.r)) * intensity) - 0.1, 0.0);
     vec3 colorFoam = (1.0 - texture2D(iChannel1, vec2(vUv.x, vUv.y - time * 0.2)).rgb) * displacement;
 
     vec4 colorOutput = vec4( 0.0 );
