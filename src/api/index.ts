30c15f6a96baccb56d1f4dd51322d6d88338ec19
Rodion <www.rodionkochetkow@gmail.com>
2021-04-13 17:39:47 +0300

    debug: debug

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,24 +1,25 @@
 const init = (game: Phaser.Game) => {
   game.registry.set("prevScore", 0);
   game.registry.set("gradingLevel", 5);
-  window.parent.postMessage({ eventName: "onReady", name: window.name }, "*");
-};
+  window.parent.postMessage({ eventName: "onReady", id: window.name }, "*");
 
-const onMessageHanlder = (game: Phaser.Game) => {
   window.addEventListener("message", (e: MessageEvent) => {
+    console.log("inside_game_e.data.eventName", e.data.prevScore, e.data.gradingLevel);
     if (e.data.eventName === "setCurrentState") {
+      console.log("inside_game_e.data.eventName", e.data.prevScore, e.data.gradingLevel);
       game.registry.set("prevScore", e.data.prevScore);
       game.registry.set("gradingLevel", e.data.gradingLevel);
     }
   });
+  debugger;
 };
 
 const onGameOver = (game: Phaser.Game, score: number) => {
   if (game.registry.get("prevScore") <= score) {
     game.registry.set("prevScore", score);
   }
-
-  window.parent.postMessage({ eventName: "updateScore", score }, "*");
+  debugger;
+  window.parent.postMessage({ eventName: "updateScore", id: window.name, score }, "*");
 };
 
-export default { init, onMessageHanlder, onGameOver };
+export default { init, onGameOver };
